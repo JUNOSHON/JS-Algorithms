82956837a72b9ebb2214131aa2758a6b9da01a1e
sanoldfox@naver.com <sanoldfox@naver.com>
2023-01-16 15:35:28 +0900

    일곱난쟁이 미완성

---
+++ src/Algorithm/7dwarf.js
@@ -0,0 +1,26 @@
+
+function solution(n){
+
+    const nineDwarf=new Array(n);
+    const sevenDwarf=new Array(n); //정답 난쟁이
+    const heightSum = 0;
+
+    for(let i = 0; i<n;i++){
+        nineDwarf[i] = Math.floor(Math.random() * 100) + 1;
+    }
+    console.log(nineDwarf);
+    while(heightSum != 100) {//키의 합이 100이 아니면 계속 반복
+
+        
+
+
+        
+
+    }
+    
+
+
+
+}
+
+solution(9);

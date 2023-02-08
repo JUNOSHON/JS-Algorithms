2a566a194a896663efb02e502f48021d6ea51586
junoshon <sanoldfox@naver.com>
2023-02-08 15:56:53 +0900

    reduce

---
+++ src/ES6/reduce.js
@@ -0,0 +1,15 @@
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce((acc, cur) => {
+//   console.log(acc, cur);
+//   return acc + cur;
+// }, 10);
+
+// console.log(sum);
+
+const juno = ["JUNO", " is sexy", " and cool"];
+
+const superjuno = juno.reduce((acc, cur) => {
+  return acc + cur;
+});
+
+console.log(superjuno);

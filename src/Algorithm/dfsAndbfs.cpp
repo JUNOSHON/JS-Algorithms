1a1adf499a550eab8ac49694eb34588882cb272f
junoshon <sanoldfox@naver.com>
2023-07-22 21:17:15 +0900

    dfsbfs

---
+++ src/Algorithm/dfsAndbfs.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> vec[10002];
+vector<int> result_bfs;
+vector<int> result_dfs;
+bool visited[1002];
+
+void bfs(int tmp)
+{
+  queue<int> q;
+  q.push(tmp);
+  visited[tmp] = true;
+  while (!q.empty())
+  {
+    int x = q.front();
+    q.pop();
+    result_bfs.push_back(x);
+
+    for (int i = 0; i < vec[x].size(); i++)
+    {
+      if (!visited[vec[x][i]])
+      {
+        q.push(vec[x][i]);
+        visited[vec[x][i]] = true;
+      }
+    }
+  }
+}
+void dfs(int x)
+{
+  visited[x] = true;
+  result_dfs.push_back(x);
+
+  for (int i = 0; i < vec[x].size(); i++)
+  {
+    if (!visited[vec[x][i]])
+    {
+      dfs(vec[x][i]);
+    }
+  }
+}
+
+int main()
+{
+  int n, m, v, a, b;
+  cin >> n >> m >> v;
+
+  for (int i = 1; i <= m; i++)
+  {
+    cin >> a >> b;
+    vec[a].push_back(b);
+    vec[b].push_back(a);
+  }
+  for (int i = 1; i <= n; i++)
+  {
+    sort(vec[i].begin(), vec[i].end());
+  }
+  bfs(v);
+  memset(visited, false, sizeof(visited));
+  dfs(v);
+
+  for (int i = 0; i < result_dfs.size(); i++)
+  {
+    cout << result_dfs[i] << " ";
+  }
+  cout << '\n';
+  for (int i = 0; i < result_bfs.size(); i++)
+  {
+    cout << result_bfs[i] << " ";
+  }
+  return 0;
+}
